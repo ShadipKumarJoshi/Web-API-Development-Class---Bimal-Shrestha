aefc231e7a07c8edc6cbf1bfa73bf6596c574494
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-05-12 13:47:09 +0545

    ff

--- Frontend/src/apis/api.js
+++ Frontend/src/apis/api.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 // Creating backend Config!
 const Api = axios.create({
-    baseURL : "http://localhost:5000",
-    withCredentials : true,
-    headers :{
-        "Content-Type" : "application/json"
+    baseURL: "http://localhost:5000",
+    withCredentials: true,
+    headers: {
+        "Content-Type": "application/json"
     }
 })
 
@@ -13,9 +13,9 @@ const Api = axios.create({
 export const testApi = () => Api.get('/test') // same as in backend  /test
 
 // Register API
-export const registerUserApi = (data) => Api.post('api/user/create',data) // (data) from frontend is received and sent to data in backend
+export const registerUserApi = (data) => Api.post('api/user/create', data) // (data) from frontend is received and sent to data in backend
 
 // Login API
-export const loginUserApi = (data) => Api.post('api/user/login',data)
+export const loginUserApi = (data) => Api.post('api/user/login', data)
 
 // http://localhost:5000/test
