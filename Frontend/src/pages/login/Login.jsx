acdf334031f64b559efbb401f02823f1b6d0f055
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-05-15 10:30:48 +0545

    email,password validation

--- Frontend/src/pages/login/Login.jsx
+++ Frontend/src/pages/login/Login.jsx
@@ -6,23 +6,48 @@
 //Make a function to handle the form submission
 
 
-import React, {useState} from 'react'
-import Navbar from "../../components/Navbar";
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 
 const Login = () => {
 
-    //make a useState fir each input
-    const [email,setEmail] = useState('')
-    const [password,setPassword]= useState('')
+    //make a useState f0r each input
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     // make a error state
-    const [emailError,setEmailError] = useState('')
-    const [passwordError,setPasswordError]= useState('')
-    
+    const [emailError, setEmailError] = useState('')
+    const [passwordError, setPasswordError] = useState('')
+
+    // Email/password Validation
+    const validation = () => {
+        let isValid = true;
+        if (email.trim() === "" || !email.includes('@')) {
+            setEmailError("Email is empty or invalid!")
+            isValid = false;
+        }
+
+        if (password.trim() === "") {
+            setPasswordError("Password is empty!")
+            isValid = false;
+        }
+
+        return isValid;
+
+    }
+
+
+
+
     //Make a function to handle the form submission
     const handleLogin = (e) => {
         e.preventDefault()
+
+        // Validation from email/password
+        if (!validation()) {
+            return
+        }
+
         toast.success('Login button is clicked!')
     }
 
@@ -30,23 +55,31 @@ const Login = () => {
     return (
         <div className="container">
             <h1>Login to your  Account!       </h1>
-            <form className='w-50'> 
+            <form className='w-50'>
                 <label>Email Address: {email} </label>
-                <input  onChange={(e) => setEmail(e.target.value)} type="text" className="form-control" placeholder="Enter your email" />
+                <input onChange={(e) => setEmail(e.target.value)} type="text" className="form-control" placeholder="Enter your email" />
+
+                {
+                    emailError && <p className='text-danger'>{emailError}</p>
+                }
 
                 <label>Password</label>
                 <input onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" placeholder="Enter your password"></input>
+
+                {
+                    passwordError && <p className='text-danger'>{passwordError}</p>
+                }
 
                 <button onClick={handleLogin} className='btn btn-danger w-100 mt-3'> Login</button> // button is onClick here in login
 
 
             </form>
 
-           
-           
+
+
         </div>
     )
 }
 
-export default Login; 
+export default Login;
 
